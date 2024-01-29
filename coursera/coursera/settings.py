54ec06dc8494ed11407cf11d7d04d815be2080da
Paul Krzyz <pekrzyz@yahoo.com>
2024-01-28 20:38:31 -0500

    added comment to settings.py

--- coursera/coursera/settings.py
+++ coursera/coursera/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# comment added by Paul
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
