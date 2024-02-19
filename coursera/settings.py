e67af91117df40f19d08feb379ce6e6deb0f61fd
Raptor <redhat.raptor@gmail.com>
2024-02-18 22:25:08 -0500

    Add production deployment code

--- coursera/settings.py
+++ coursera/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+# For reading Postgres database URL
+import dj_database_url
+
 load_dotenv()
 
 # comment added by Paul
@@ -29,17 +32,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.environ.get("DEBUG", "False").upper() == "TRUE"
+LOCAL = os.environ.get("LOCAL", "False").upper() == "TRUE"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.getenv('DEBUG')
+SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT", "True").upper() == "TRUE"
+SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE", "True").upper() == "TRUE"
+CSRF_COOKIE_SECURE = os.environ.get("CSRF_COOKIE_SECURE", "True").upper() == "TRUE"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-ALLOWED_HOSTS = []
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "")
+if ALLOWED_HOSTS:
+    ALLOWED_HOSTS = [x.strip() for x in ALLOWED_HOSTS.split(",")]
+else:
+    ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "")
+if CSRF_TRUSTED_ORIGINS:
+    CSRF_TRUSTED_ORIGINS = [x.strip() for x in CSRF_TRUSTED_ORIGINS.split(",")]
+else:
+    CSRF_TRUSTED_ORIGINS = []
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,6 +78,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     "corsheaders.middleware.CorsMiddleware",
@@ -73,34 +93,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'coursera.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'coursera.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / os.getenv('DATABASE'),
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=not LOCAL)}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -141,16 +141,25 @@ USE_I18N = True
 
 USE_TZ = True
 
+# For serving Angular files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/ng/"
+# Path to Angular
+ANGULAR_APP_DIR = f'{os.path.abspath(os.path.dirname(__name__))}/my-app/dist/my-app'
+WHITENOISE_ROOT = ANGULAR_APP_DIR
+WHITENOISE_INDEX_FILE = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(ANGULAR_APP_DIR),
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ORIGIN_WHITELIST", "").split(",")
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -158,7 +167,21 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-    'https://www.raptorcoders.com',
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        "DIRS": [
+            ANGULAR_APP_DIR
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
