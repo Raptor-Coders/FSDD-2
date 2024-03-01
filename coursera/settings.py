daebf586f920b9622aaf800297760bb94a80d349
Raptor <redhat.raptor@gmail.com>
2024-02-29 20:49:28 -0500

    Update settings and heroku.yml

--- coursera/settings.py
+++ coursera/settings.py
@@ -99,14 +99,14 @@ WSGI_APPLICATION = 'coursera.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=not LOCAL)}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=not LOCAL)}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
 
